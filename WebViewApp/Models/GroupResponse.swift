178ae400dccc7fae90756d4fcc1afde46bcb8525
Vsevolod <vsevolod.lashin@gmail.com>
2023-07-09 23:56:48 +0400

    Group Model changed

--- WebViewApp/Models/GroupResponse.swift
+++ WebViewApp/Models/GroupResponse.swift
@@ -11,5 +11,19 @@ struct GroupResponse: Decodable {
 
 struct Groups: Decodable {
     let count: Int
-    let items: [Int]
+    let items: [Group]
+}
+
+struct Group: Decodable {
+    let id: String
+    let description: String
+    let name: String
+    let photoOneHundred: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case description
+        case name
+        case photoOneHundred = "photo_100"
+    }
 }
