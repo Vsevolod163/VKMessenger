03472afef6749cd61d7f716bc72e25bbfc55b81d
Vsevolod <vsevolod.lashin@gmail.com>
2023-07-10 01:16:43 +0400

    GroupsVC code added

--- WebViewApp/ViewControllers/GroupsViewController.swift
+++ WebViewApp/ViewControllers/GroupsViewController.swift
@@ -12,20 +12,42 @@ final class GroupsViewController: UITableViewController {
     var token: String!
     var userID: String!
     
+    private let cellID = "group"
     private let networkManager = NetworkManager.shared
+    private var groups: [Group] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(GroupViewCell.self, forCellReuseIdentifier: cellID)
         fetchData()
+        tableView.rowHeight = 130
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        groups.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        guard let cell = cell as? GroupViewCell else { return UITableViewCell() }
+        
+        cell.selectionStyle = .none
+        let group = groups[indexPath.row]
+        cell.configure(with: group)
+        
+        return cell
     }
     
     private func fetchData() {
         let groupUrl = URL(string: "https://api.vk.com/method/groups.get?user_id=\(userID ?? "")&extended=1&fields=description&access_token=\(token ?? "")&v=5.131")!
         
-        networkManager.fetch(GroupResponse.self, from: groupUrl) { result in
+        networkManager.fetch(GroupResponse.self, from: groupUrl) { [weak self] result in
             switch result {
             case .success(let group):
-                print(group.response.items)
+                self?.groups = group.response.items
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
             case .failure(let error):
                 print(error)
             }
--- WebViewApp/Views/GroupViewCell.swift
+++ WebViewApp/Views/GroupViewCell.swift
@@ -6,7 +6,77 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class GroupViewCell: UITableViewCell {
-
+    private lazy var groupImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    private lazy var groupNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 14)
+        
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupSubviews(groupImageView, groupNameLabel, descriptionLabel)
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with group: Group) {
+        groupNameLabel.text = group.name
+        descriptionLabel.text = group.description
+        groupImageView.kf.setImage(with: URL(string: group.photoOneHundred))
+    }
+    
+    private func setConstraints() {
+        groupImageView.translatesAutoresizingMaskIntoConstraints = false
+        groupNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(
+            [
+                groupImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+                groupImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+                groupImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+                groupImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.3),
+                
+                groupNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+                groupNameLabel.leadingAnchor.constraint(equalTo: groupImageView.trailingAnchor, constant: 8),
+                groupNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+                
+                descriptionLabel.topAnchor.constraint(equalTo: groupNameLabel.bottomAnchor, constant: 8),
+                descriptionLabel.leadingAnchor.constraint(equalTo: groupImageView.trailingAnchor, constant: 8),
+                descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+                descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+            ]
+        )
+    }
+    
+    private func setupSubviews(_ subviews: UIView...) {
+        subviews.forEach { subview in
+            contentView.addSubview(subview)
+        }
+    }
 }
