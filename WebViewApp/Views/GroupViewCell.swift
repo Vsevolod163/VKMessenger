be744bfd735d574cfd71362a48cba9045304d012
Vsevolod <vsevolod.lashin@gmail.com>
2023-07-10 00:03:18 +0400

    GroupViewCell added

---
+++ WebViewApp/Views/GroupViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupViewCell.swift
+//  WebViewApp
+//
+//  Created by Vsevolod Lashin on 10.07.2023.
+//
+
+import UIKit
+
+class GroupViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
