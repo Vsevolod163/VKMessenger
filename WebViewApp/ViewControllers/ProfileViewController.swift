53f1ebf9354c7eadd848f001288c0c2b8504564b
Vsevolod <vsevolod.lashin@gmail.com>
2023-07-14 14:38:17 +0400

    func fetchUser added

--- WebViewApp/ViewControllers/ProfileViewController.swift
+++ WebViewApp/ViewControllers/ProfileViewController.swift
@@ -9,8 +9,26 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
 
+    private let networkManager = NetworkManager.shared
+    private var items: [Item] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .black
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+    private func fetchUser() {
+        let url = "https://api.vk.com/method/users.get?user_ids=\(NetworkManager.userID)&fields=city,online,photo_100&access_token=\(NetworkManager.token)&v=5.131"
+        guard let url = URL(string: url) else { return }
+        
+        networkManager.fetch(FriendResponse.self, from: url) { [weak self] result in
+            switch result {
+            case .success(let response):
+                self?.items = response.response.items
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
