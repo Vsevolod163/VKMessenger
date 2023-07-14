791d64df9be8ed3c64dbb4149c5144251434ab4a
Vsevolod <vsevolod.lashin@gmail.com>
2023-07-14 12:18:55 +0400

    ProfileVC added

---
+++ WebViewApp/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  WebViewApp
+//
+//  Created by Vsevolod Lashin on 14.07.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
