cf094cc1b371301b14596c9b30e535e998813e0b
Vsevolod <vsevolod.lashin@gmail.com>
2023-07-15 00:59:29 +0400

    ChatVC added

---
+++ WebViewApp/ViewControllers/ChatViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChatViewController.swift
+//  WebViewApp
+//
+//  Created by Vsevolod Lashin on 15.07.2023.
+//
+
+import UIKit
+
+final class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
