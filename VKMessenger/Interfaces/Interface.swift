bf3fd9541296bd7521dd4d31b895c30e81f65948
Vsevolod <vsevolod.lashin@gmail.com>
2023-07-22 09:42:52 +0400

    Interface class added

---
+++ VKMessenger/Interfaces/Interface.swift
@@ -0,0 +1,18 @@
+//
+//  Interface.swift
+//  VKMessenger
+//
+//  Created by Vsevolod Lashin on 22.07.2023.
+//
+
+import Foundation
+import UIKit
+
+final class Interface {
+    static var shared = Interface()
+    static var viewColor: UIColor = .white
+    static var cellColor: UIColor = .white
+    static var textColor: UIColor = .black
+    
+    private init() {}
+}
